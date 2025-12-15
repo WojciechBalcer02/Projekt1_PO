6cf20b9b309591d1f000e7d5c5f2905bec42d641
Wojciech <wojciechbalcer02@gmail.com>
2025-12-15 16:07:06 +0100

    dodanie nowej klasy abstrakcyjnej Room

---
+++ src/main/java/hostel/domain/Room.java
@@ -0,0 +1,37 @@
+package hostel.domain;
+
+public abstract class Room {
+    private String id;
+    private int beds;
+    private boolean occupied;
+
+    //konstruktor
+    public Room(String id, int beds){
+        this.id = id;
+        this.beds = beds;
+        this.occupied = false;
+    }
+
+    public boolean isOccupied(){
+        return occupied;
+    }
+
+    public void occupy(){
+        this.occupied = true;
+    }
+
+    public void release(){
+        this.occupied = false;
+    }
+
+    //abstract by każda klasa miała swój nadpisany opis i musiała go mieć
+    public abstract String description();
+
+    public String getId() {
+        return id;
+    }
+
+    public int getBeds() {
+        return beds;
+    }
+}
